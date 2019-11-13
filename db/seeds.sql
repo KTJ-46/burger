796daf443dc9effa39dcb858c9aa353ea2dbb8a7
Kshitij Vadake <ktj@Kshitijs-MBP.fios-router.home>
2019-11-12 23:46:15 -0500

    final working version

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES (' Veggie Burger', false);
